d3dbb05579249d614c48d12a24507f88219c2f09
Nguyenduclong99 <55075056+Nguyenduclong99@users.noreply.github.com>
2020-09-25 10:31:03 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './share/header/header.component';
+import { FooterComponent } from './share/footer/footer.component';
+
+import { MainComponent } from './main/main.component';
+import { ShareComponent } from './share/share.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './product/cart/cart.component';
+import { DetailComponent } from './product/detail/detail.component';
+import { ListItemComponent } from './product/list-item/list-item.component';
+import { SearchComponent } from './search/search.component';
+import { ModalComponent } from './share/modal/modal.component';
+import { LoginComponent } from './login/login.component';
+import { CheckoutComponent } from './product/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+
+    MainComponent,
+
+    ShareComponent,
+
+    ProductComponent,
+
+    CartComponent,
+
+    DetailComponent,
+
+    ListItemComponent,
+
+    SearchComponent,
+
+    ModalComponent,
+
+    LoginComponent,
+
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
